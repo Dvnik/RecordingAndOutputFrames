6c97f57167963828e6c8a9222c096b7bd75c722a
dvnik147 <dvnik147@hotmail.com>
2022-11-14 10:07:23 +0800

    test function create

---
+++ RecordingAndOutputFrames/Handler/MediaHandler.swift
@@ -0,0 +1,97 @@
+//
+//  MediaHandler.swift
+//  JunkTest
+//
+//  Created on 2022/11/10.
+//
+
+import Foundation
+import AVFoundation
+import AVKit
+
+class MediaHandler: NSObject {
+    //MARK: Singleton
+    public static var shared:MediaHandler {
+        get {
+            if handler == nil { handler = MediaHandler() }
+            return handler
+        }
+    }
+    private static var handler:MediaHandler!
+    //MARK:Value
+    var tempOutputPath: String {
+        get {
+            return NSTemporaryDirectory() + "output.mov"
+        }
+    }
+    
+    //AVFoundation
+    let captureSession = AVCaptureSession()
+    var videoFileOutput = AVCaptureMovieFileOutput()// 設置影片擷取後輸出的session
+    
+    var currentDevice: AVCaptureDevice!//必須找到相機裝置來拍攝影片
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?//預覽層（preview layer）做為相機預覽用
+    
+    //MARK: - functions
+    //MARK: default setting
+    func defaultConfigure(showView: UIView) -> Bool {
+        // 取得後置相機來擷取影片
+        currentDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+        // 取得輸入資料源
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else {
+            return false
+        }
+        // 以高解析度來預設 session
+        captureSession.sessionPreset = .high
+        // 設置輸入與輸出裝置的 session
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(videoFileOutput)
+        // 提供相機預覽
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        showView.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = showView.layer.frame
+        
+        captureSession.startRunning()
+        
+        return true
+    }
+    // 執行錄影的功能
+    func defaultRecordMedia(isRecording status: Bool, recordingDelegate delegate: AVCaptureFileOutputRecordingDelegate) {
+        if status {
+            let outputURL = URL(fileURLWithPath: tempOutputPath)
+            videoFileOutput.startRecording(to: outputURL, recordingDelegate: delegate)
+        }
+        else {
+            videoFileOutput.stopRecording()
+        }
+    }
+    //MARK: viewSet
+    func boundingPlayerViewController(in view: UIView) -> AVPlayerViewController {
+        let avpVC = AVPlayerViewController()
+        avpVC.view.frame = view.bounds
+        
+        view.addSubview(avpVC.view)
+        return avpVC
+    }
+    //錄影的按鈕動畫
+    func recordingAnimation(isRecording status: Bool, playButton view: UIView) {
+        if status {
+            UIView.animate(withDuration: 0.5, delay: 0.3, options:  [.repeat, .autoreverse, .allowUserInteraction]) {
+                view.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }
+        }
+        else {
+            UIView.animate(withDuration: 0.5, delay: 0.0) {
+                view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            } completion: { result in
+                view.layer.removeAllAnimations()
+            }
+
+        }
+        
+    }
+    
+    
+    
+}
--- RecordingAndOutputFrames/ViewController.swift
+++ RecordingAndOutputFrames/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    //MARK: outlet
+    @IBOutlet weak var recordPreview: UIView!
+    @IBOutlet weak var btnRecord: UIButton!
+    
+    @IBOutlet weak var playerView: UIView!
+    
+    @IBOutlet weak var lblClickInfo: UILabel!
+    //MARK: values
+    let handler = MediaHandler.shared
+    
+    var avPlayerVC: AVPlayerViewController!
+    var tempOutputPath: URL!
 
+    var isRecording: Bool = false
+    //MARK: life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lblClickInfo.text = ""
+        _ = handler.defaultConfigure(showView: recordPreview)
+        avPlayerVC = handler.boundingPlayerViewController(in: playerView)
     }
+    //MARK: @IBAction
 
-
+    @IBAction func actionRecording(_ sender: UIButton) {
+        self.isRecording = !self.isRecording
+        handler.recordingAnimation(isRecording: self.isRecording, playButton: sender)
+        handler.defaultRecordMedia(isRecording: self.isRecording, recordingDelegate: self)
+    }
+    
+    
+    @IBAction func onClickAct01(_ sender: UIButton) {
+        lblClickInfo.text = "\(#function)"
+    }
+    
+    @IBAction func onClickAct02(_ sender: UIButton) {
+        lblClickInfo.text = "\(#function)"
+    }
+    
+    @IBAction func onClickAct03(_ sender: UIButton) {
+        lblClickInfo.text = "\(#function)"
+    }
 }
 
+//MARK: - AVCaptureFileOutputRecordingDelegate
+extension ViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        guard error == nil else {
+            print(error ?? "")
+            return
+        }
+        
+        tempOutputPath = outputFileURL
+        avPlayerVC.player = AVPlayer(url: outputFileURL)
+    }
+    
+    
+    
+    
+    
+}
